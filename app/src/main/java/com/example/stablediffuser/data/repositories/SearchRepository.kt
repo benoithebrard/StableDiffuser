e18238648bda7873d1582980931a1f12163ed966
Benoit Hebrard <benoit@shapegames.com>
2022-12-12 20:18:28 +0100

    caching complete

--- app/src/main/java/com/example/stablediffuser/data/repositories/SearchRepository.kt
+++ app/src/main/java/com/example/stablediffuser/data/repositories/SearchRepository.kt
@@ -10,11 +10,14 @@ import okhttp3.MediaType.Companion.toMediaType
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import java.io.IOException
+import java.util.*
 
 class SearchRepository(
     baseUrl: String,
     httpClientBuilder: OkHttpClient.Builder
 ) {
+    private val searchCache: WeakHashMap<String, List<LexicaImage>> = WeakHashMap()
+
     private val lexicaJson by lazy {
         Json {
             ignoreUnknownKeys = true
@@ -36,14 +39,21 @@ class SearchRepository(
 
     suspend fun searchForImages(
         query: String
-    ): Result<List<LexicaImage>> {
-        return try {
-            lexicaService.searchForImages(query).toResult().map { searchResponse ->
-                searchResponse.images
+    ): Result<List<LexicaImage>> = searchCache[query]?.let { images ->
+        // return images from cache
+        Result.success(images)
+    } ?: try {
+        // search for images
+        lexicaService.searchForImages(query).toResult().map { searchResponse ->
+            searchResponse.images
+        }.also { result ->
+            result.getOrNull()?.let { images ->
+                // store images in cache
+                searchCache[query] = images
             }
-        } catch (e: IOException) {
-            Result.failure(e)
         }
+    } catch (e: IOException) {
+        Result.failure(e)
     }
 
 }
