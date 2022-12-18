19fc438b0ad598e303e0e59c6dd8d90bfe2e0f94
Benoit Hebrard <benoit@shapegames.com>
2022-12-18 14:26:15 +0100

    small refatcor

--- app/src/main/java/com/example/stablediffuser/MainActivity.kt
+++ app/src/main/java/com/example/stablediffuser/MainActivity.kt
@@ -10,39 +10,40 @@ import androidx.navigation.ui.setupWithNavController
 import com.example.stablediffuser.config.Configuration
 import com.example.stablediffuser.databinding.ActivityMainBinding
 import com.example.stablediffuser.utils.NavOptionsHelper.popToSearchNavOptions
-import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var binding: ActivityMainBinding
+    private lateinit var viewBinding: ActivityMainBinding
 
     private val navController: NavController by lazy {
         findNavController(R.id.nav_host_fragment_activity_main)
+    }
+
+    private val appBarConfiguration by lazy {
+        AppBarConfiguration(
+            topLevelDestinationIds = setOf(R.id.search_dest, R.id.favorites_dest)
+        )
     }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         Configuration.provideAppContext = { applicationContext }
 
-        binding = ActivityMainBinding.inflate(layoutInflater)
-        setContentView(binding.root)
+        viewBinding = ActivityMainBinding.inflate(layoutInflater)
 
-        val navView: BottomNavigationView = binding.navView
-
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
-        val appBarConfiguration = AppBarConfiguration(
-            setOf(R.id.search_dest, R.id.favorites_dest)
-        )
+        setContentView(viewBinding.root)
 
         setupActionBarWithNavController(navController, appBarConfiguration)
 
-        navView.setupWithNavController(navController)
+        with(viewBinding.navView) {
+            setupWithNavController(navController)
 
-        navView.setOnItemReselectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.search_dest -> {
-                    navController.navigate(R.id.search_dest, null, popToSearchNavOptions)
+            setOnItemReselectedListener { menuItem ->
+                when (menuItem.itemId) {
+                    R.id.search_dest -> {
+                        // Search was reselected, pop all fragments
+                        navController.navigate(R.id.search_dest, null, popToSearchNavOptions)
+                    }
                 }
             }
         }
