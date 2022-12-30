a121d27fc1031b9dd4cc05f45260c78ccea6b639
Benoit Hebrard <benoit@shapegames.com>
2022-12-30 11:35:18 +0100

    add hilt to app

---
+++ app/src/main/java/com/benoithebrard/stablediffuser/StableDiffuserApplication.kt
@@ -0,0 +1,7 @@
+package com.benoithebrard.stablediffuser
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class StableDiffuserApplication : Application()
